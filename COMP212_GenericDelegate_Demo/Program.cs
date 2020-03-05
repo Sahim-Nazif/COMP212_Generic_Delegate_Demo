a77c2cd343a2039c98dcb31c319dcb11e2ab7e6f
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-05 17:48:21 -0500

    created delegate reference-called methods

--- COMP212_GenericDelegate_Demo/Program.cs
+++ COMP212_GenericDelegate_Demo/Program.cs
@@ -8,7 +8,22 @@ namespace COMP212_GenericDelegate_Demo
     {
         static void Main(string[] args)
         {
-           
+
+            add<int> Sum = AddNumber;
+            Console.WriteLine("Result of addition is:" + Sum(12,33));
+
+            add<string> JoinStrings= Concat;
+            Console.WriteLine(JoinStrings("This is " , "Generic Delegate Demo"));
+
+
+        }
+        public static int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+        public static string Concat(string a, string b)
+        {
+            return a + b;
         }
     }
 }
