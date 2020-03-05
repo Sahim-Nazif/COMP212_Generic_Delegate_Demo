b7eaef97404a2532cfd875843489bde42b2301ac
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-03-05 17:40:29 -0500

    created a generic delegate

--- COMP212_GenericDelegate_Demo/Program.cs
+++ COMP212_GenericDelegate_Demo/Program.cs
@@ -2,11 +2,13 @@
 
 namespace COMP212_GenericDelegate_Demo
 {
+    //creating a generic delegate 
+    public delegate T add<T>(T a, T b);
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           
         }
     }
 }
